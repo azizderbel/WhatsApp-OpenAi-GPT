41a570bc2e35529169005af8696a4c9d8c8ee624
azizderbel <mohamedaziz.derbel@esprit.tn>
2023-02-22 15:04:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
+
+
+const client = new Client();
+console.log(process.env.OPENAI_API_KEY)
+client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion (message) {
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: message,
+        max_tokens: 200,
+    });
+    return completion.data.choices[0].text;
+}
+
+client.on('message', message => {
+
+    if(message.body.startsWith("To bot:")) {
+        runCompletion(message.body.substring(1)).then(result => message.reply(result));
+    }
+});
